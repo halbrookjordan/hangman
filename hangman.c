a3dd3b9c85cc67bb8e2458c1acd6aa85504e191f
Jordan Halbrook <halb5164@kettering.edu>
2020-03-10 11:33:27 -0400

    Added selection for single or multiplayer

--- hangman.c
+++ hangman.c
@@ -2,21 +2,46 @@
 #include <string.h>
 #include <stdlib.h>
 
-void printTitle(void);
+char printTitle(void);
+void singlePlayer(void);
+void multiPlayer(void);
 
 int main(void)
 {
-  printTitle();
+  char players = printTitle();
+
+  if(players == '1'){
+    singlePlayer();
+  }
+  else{
+    multiPlayer();
+  }
 
   return 0;
 }
 
-void printTitle(void)
+char printTitle(void)
 {
+  char players;
   system("clear");
   printf("\t***************\n");
   printf("\t H A N G M A N\n");
   printf("\t***************\n");
-  printf("\n\nPress ENTER to continue");
-  getchar();
+  printf("\n\nEnter 1 for single player or 2 for multiplayer:");
+  scanf("%c", &players);
+  
+  while(players != '1' && players != '2'){
+    printf("\nPlease enter 1 or 2:");
+    scanf(" %c", &players);
+  }
+
+  return players;
+}
+
+void singlePlayer(void)
+{
+}
+
+void multiPlayer(void)
+{
 }
