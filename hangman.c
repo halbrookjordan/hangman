1ac7b8c2ccb1b42812d38ec71a610f472a39b9c3
Jordan Halbrook <halb5164@kettering.edu>
2020-03-10 11:57:24 -0400

    Implemented quit option

--- hangman.c
+++ hangman.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include <ctype.h>
 
 char printTitle(void);
 void singlePlayer(void);
@@ -13,7 +14,7 @@ int main(void)
   if(players == '1'){
     singlePlayer();
   }
-  else{
+  else if(players == '2'){
     multiPlayer();
   }
 
@@ -27,12 +28,13 @@ char printTitle(void)
   printf("\t***************\n");
   printf("\t H A N G M A N\n");
   printf("\t***************\n");
-  printf("\n\nEnter 1 for single player or 2 for multiplayer:");
+  printf("\n\nEnter 1 for single player or 2 for multiplayer or q to quit:");
   scanf("%c", &players);
   
-  while(players != '1' && players != '2'){
-    printf("\nPlease enter 1 or 2:");
-    scanf(" %c", &players);
+  while(players != '1' && players != '2' && players != 'q'){
+    printf("\nPlease enter 1,2 or q:");
+    scanf(" %c",&players);
+    players = tolower(players);
   }
 
   return players;
